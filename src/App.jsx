a8d6f39a5248996c8866174ba8ae49a0915bbe5d
Tazzy <tanlinsir@outlook.com>
2025-02-16 16:58:06 +0000

    Blank template.

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,8 @@
-import { Canvas, useFrame } from "@react-three/fiber";
 import "./App.css";
-import { Html, ScrollControls, Scroll, OrbitControls, Environment, useScroll } from "@react-three/drei";
-import { BMWInstance } from "../components/bmw";
-import WebContent from "./webContent";
-import { use, useRef } from "react";
-
-const RotatingBMW = () => {
-  const scroll = useScroll();
-  const ref = useRef();
-
-  useFrame(() => {
-    // Get the current scroll progress (0 to 1)
-    const scrollDelta = scroll.delta;
-        
-    // Update the mesh rotation
-    if (Math.abs(scrollDelta) > 0.0001) {
-      ref.current.rotation.y += scrollDelta * Math.PI * 2;
-    }
-  });
-
-  return (
-    <mesh
-      ref={ref}
-      rotation={[0, Math.PI, 0]}
-      position={[2, -1.4, 0]}
-      scale={1.2}>
-      <BMWInstance />
-    </mesh>
-  );
-};
 
 const App = () => {
   return (
-    <Canvas>
-      <Environment preset="sunset" />
-      <WebContent />
-      <ScrollControls>
-        <Scroll>
-          <RotatingBMW />
-        </Scroll>
-      </ScrollControls>
-      
-    </Canvas>
+    <h1>Hello</h1>
   );
 };
 
